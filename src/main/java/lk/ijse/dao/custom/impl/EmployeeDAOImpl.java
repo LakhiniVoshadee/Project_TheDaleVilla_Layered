c273b0c993b2335adf5cf82e4a096f003237ac0e
Project_TheDaleVilla_MVC <lakhinivoshadee24@gmail.com>
2024-06-27 16:37:45 +0530

    EmployeeDAOImpl

--- src/main/java/lk/ijse/dao/custom/impl/EmployeeDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/EmployeeDAOImpl.java
@@ -1,7 +1,115 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.dao.SQLUtil;
 import lk.ijse.dao.custom.EmployeeDAO;
+import lk.ijse.db.Dbconnection;
+import lk.ijse.entity.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+    @Override
+    public  String generateNextId() throws SQLException, ClassNotFoundException {
+        /*String sql = "Select EmpID from employee order by EmpID desc limit 1 ";
+        Connection connection = Dbconnection.getInstance().getConnection();
+        ResultSet resultSet = connection.prepareStatement(sql).executeQuery();
+
+        String id = null;
+        if (resultSet.next()) {
+            id = resultSet.getString(1);
+            return splitId(id);
+        }
+        return splitId(null);*/
+
+        ResultSet rst = SQLUtil.execute("Select EmpID from employee order by EmpID desc limit 1");
+        if (rst.next()) {
+            String id = rst.getString("id");
+            int newEmpID = Integer.parseInt(id.replace("E00-",""))+1;
+            return String.format("E00-%s",newEmpID);
+        }else {
+            return "E00-001";
+        }
+    }
+
+   /* private static String splitId(String id) {
+        if (id != null) {
+            String[] split = id.split("Emp ");
+            int EmpId = Integer.parseInt(split[1]);
+            EmpId++;
+            return "Emp " + EmpId;
+        }
+        return "Emp 1";
+    }*/
+
+    @Override
+    public boolean save(Employee employee) throws SQLException, ClassNotFoundException {
+       /* String sql = "INSERT INTO employee VALUES (?,?,?,?,?,?)";
+        PreparedStatement pstm = Dbconnection.getInstance().getConnection().prepareStatement(sql);
+
+        pstm.setObject(1, employee.getEmpID());
+        pstm.setObject(2, employee.getName());
+        pstm.setObject(3, employee.getType());
+        pstm.setObject(4, employee.getEmail());
+        pstm.setObject(5, employee.getDOB());
+        pstm.setObject(6, employee.getUserID());
+
+        return pstm.executeUpdate() > 0;*/
+        return SQLUtil.execute("INSERT INTO employee VALUES (?,?,?,?,?,?)");
+
+    }
+
+    @Override
+    public  boolean update(Employee employee) throws SQLException, ClassNotFoundException {
+       /* String sql = "UPDATE employee SET Name = ?,Type = ?,DOB = ?,Email = ?,UserID = ? WHERE EmpId = ? ";
+        PreparedStatement pstm = Dbconnection.getInstance().getConnection().prepareStatement(sql);
+
+        pstm.setObject(1, employee.getName());
+        pstm.setObject(2, employee.getType());
+        pstm.setObject(3, employee.getDOB());
+        pstm.setObject(4, employee.getEmail());
+        pstm.setObject(5, employee.getEmpID());
+        // pstm.setObject(6, employee.getUserID());
+
+        return pstm.executeUpdate() > 0;*/
+
+        return SQLUtil.execute("UPDATE employee SET Name = ?,Type = ?,DOB = ?,Email = ?,UserID = ? WHERE EmpId = ?");
+    }
+
+
+
+
+  /*  public List<Employee> getEmployee() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM employee";
+        ResultSet resultSet = Dbconnection.getInstance().getConnection().prepareStatement(sql).executeQuery();
+        List<Employee> employeeList = new ArrayList<>();
+        while (resultSet.next()) {
+            employeeList.add(new Employee(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getString(5),
+                    resultSet.getString(6)
+
+            ));
+        }
+        return employeeList;
+    }
+*/
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+       /* String sql = "DELETE FROM employee WHERE EmpID = ?";
+        PreparedStatement pstm = Dbconnection.getInstance().getConnection().prepareStatement(sql);
+
+        pstm.setObject(1,id);
+
+        return pstm.executeUpdate() > 0;*/
+        return SQLUtil.execute("DELETE FROM employee WHERE EmpID = ?");
+    }
 }
